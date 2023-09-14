f6f1173f505a17433ae074033a636ca165b2de9b
Sawa <34698182+sawaYch@users.noreply.github.com>
2023-09-14 10:12:47 +0800

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,234 @@
-import Image from 'next/image'
+"use client";
+import Image from "next/image";
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  getKeyValue,
+  Input,
+  Button,
+  Selection,
+} from "@nextui-org/react";
+import { AnimatePresence, motion } from "framer-motion";
+import { Key, useCallback, useMemo, useState } from "react";
+import pack from "../package.json";
+
+const rows = [
+  {
+    key: "1",
+    name: "Tony Reichert",
+    pic: "https://yt4.ggpht.com/ytc/AOPolaRyc4u5TCsT76h1lNuQYPbJOzEK9OTdrhguwB6d=s32-c-k-c0x00ffffff-no-rj",
+    message: "CEO",
+  },
+  {
+    key: "2",
+    name: "Zoey Lang",
+    pic: "https://yt4.ggpht.com/ytc/AOPolaRyc4u5TCsT76h1lNuQYPbJOzEK9OTdrhguwB6d=s32-c-k-c0x00ffffff-no-rj",
+    message: "Technical Lead",
+  },
+  {
+    key: "3",
+    name: "Jane Fisher",
+    pic: "https://yt4.ggpht.com/ytc/AOPolaRyc4u5TCsT76h1lNuQYPbJOzEK9OTdrhguwB6d=s32-c-k-c0x00ffffff-no-rj",
+    message: "Senior Developer",
+  },
+  {
+    key: "4",
+    name: "William Howard",
+    pic: "https://yt4.ggpht.com/ytc/AOPolaRyc4u5TCsT76h1lNuQYPbJOzEK9OTdrhguwB6d=s32-c-k-c0x00ffffff-no-rj",
+    message: "Community Manager",
+  },
+  {
+    key: "5",
+    name: "Long long long long long long name test",
+    pic: "https://yt4.ggpht.com/ytc/AOPolaRyc4u5TCsT76h1lNuQYPbJOzEK9OTdrhguwB6d=s32-c-k-c0x00ffffff-no-rj",
+    message:
+      "I am going to do it. I have made up my mind. These are the first few words of the new… the best … the Longest Text In The Entire History Of The Known Universe! This Has To Have Over 35,000 words the beat the current world record set by that person who made that flaming chicken handbooky thingy. I might just be saying random things the whole time I type in this so you might get confused a lot. I just discovered something terrible. autocorrect is on!! no!!! this has to be crazy, so I will have to break all the English language rules and the basic knowledge of the average human being. I am not an average human being, however I am special. no no no, not THAT kind of special ;). Why do people send that wink face! it always gives me nightmares! it can make a completely normal sentence creepy. imagine you are going to a friend’s house, so you text this: [ see you soon 🙂 ] seems normal, right? But what is you add the word semi to that colon? (Is that right? or is it the other way around) what is you add a lorry to that briquettes? (Semi-truck to that coal-on) anyway, back to the point: [ see you soon 😉 ]THAT IS JUST SO CREEPY! is that really your friend, or is it a creepy stalker watching your every move? Or even worse, is it your friend who is a creepy stalker? maybe you thought it was your friend, but it was actually your fri end (let me explain: you are happily in McDonalds, getting fat while eating yummy food and some random dude walks up and blots out the sun (he looks like a regular here) you can’t see anything else than him, so you can’t try to avoid eye contact. he finishes eating his cheeseburger (more like horseburgher(I learned that word from the merchant of Venice(which is a good play(if you can understand it(I can cause I got a special book with all the words in readable English written on the side of the page(which is kinda funny because Shakespeare was supposed to be a good poet but no-one can understand him(and he’s racist in act 2 scene1 of the play too))))))) and sits down beside you , like you are old pals (you’ve never met him before but he looks like he could be in some weird cult) he clears his throat and asks you a very personal question. “can i have some French fries?” (I don’t know why there called French fries when I’ve never seen a French person eat fries! all they eat it is stuff like baguettes and crêpes and rats named ratty-two-ee which is a really fun game on the PlayStation 2) And you think {bubbly cloud thinking bubble} “Hahahahahhahahahahahahahaha!!!!!!!!!!!!",
+  },
+  {
+    key: "6",
+    name: "Long long long long long long name test",
+    pic: "https://yt4.ggpht.com/ytc/AOPolaRyc4u5TCsT76h1lNuQYPbJOzEK9OTdrhguwB6d=s32-c-k-c0x00ffffff-no-rj",
+    message: "88米88貓",
+  },
+  {
+    key: "7",
+    name: "Long long long long long long name test",
+    pic: "https://yt4.ggpht.com/ytc/AOPolaRyc4u5TCsT76h1lNuQYPbJOzEK9OTdrhguwB6d=s32-c-k-c0x00ffffff-no-rj",
+    message: "88米88貓",
+  },
+  {
+    key: "8",
+    name: "Long long long long long long name test",
+    pic: "https://yt4.ggpht.com/ytc/AOPolaRyc4u5TCsT76h1lNuQYPbJOzEK9OTdrhguwB6d=s32-c-k-c0x00ffffff-no-rj",
+    message: "88米88貓",
+  },
+  {
+    key: "9",
+    name: "Long long long long long long name test",
+    pic: "https://yt4.ggpht.com/ytc/AOPolaRyc4u5TCsT76h1lNuQYPbJOzEK9OTdrhguwB6d=s32-c-k-c0x00ffffff-no-rj",
+    message: "88米88貓",
+  },
+  {
+    key: "10",
+    name: "Long long long long long long name test",
+    pic: "https://yt4.ggpht.com/ytc/AOPolaRyc4u5TCsT76h1lNuQYPbJOzEK9OTdrhguwB6d=s32-c-k-c0x00ffffff-no-rj",
+    message: "88米88貓",
+  },
+];
+
+const columns = [
+  {
+    key: "name",
+    label: "NAME",
+    width: 200,
+  },
+  {
+    key: "message",
+    label: "MESSAGE",
+  },
+];
 
 export default function Home() {
+  const [liveUrl, setLiveUrl] = useState<string | undefined>();
+  const [urlInputValue, setUrlInputValue] = useState("");
+  const [readByeBye, setReadByeBye] = useState<Set<Key>>(new Set([]));
+
+  const isReady = useMemo(() => {
+    return liveUrl != null && liveUrl.length > 0;
+  }, [liveUrl]);
+
+  const handleUrlChange = useCallback(() => {
+    if (isReady) {
+      setLiveUrl(undefined);
+    } else {
+      setLiveUrl(urlInputValue);
+    }
+  }, [urlInputValue, isReady]);
+  const appVersion = useMemo(() => {
+    return pack.version;
+  }, []);
+
+  const handleReadByeBye = useCallback(
+    (keys: Selection) => {
+      if (keys == "all") return;
+      // not allow to un-tick
+      if (keys.size < readByeBye.size) return;
+      setReadByeBye(keys);
+    },
+    [readByeBye]
+  );
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex flex-col min-h-screen items-center px-10 font-mono">
+      <div className="flex self-end mt-2 relative">
+        <div className="flex flex-col items-center justify-center text-xs">
+          さようなら Mya {appVersion ?? "unknown version"}
+          <div>Created By No.159</div>
+          <div>Sawa</div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="flex place-items-center p-2"
+          href="https://github.com/sawaYch"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
+          <Image
+            className="rounded-full"
+            src="/author.jpg"
+            alt="author"
+            width={32}
+            height={32}
+            priority
+          />
         </a>
       </div>
+      <motion.div
+        className="justify-center flex flex-col w-full grow gap-4"
+        layout
+      >
+        <AnimatePresence>
+          <motion.div
+            key="url-input-bar"
+            layout
+            className="flex justify-center items-center gap-2 "
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            <Input
+              type="url"
+              isClearable={!isReady}
+              label="Enter Youtube Live Url"
+              placeholder="for example: https://www.youtube.com/watch?v=7LA9mAOz7gA"
+              radius="lg"
+              value={urlInputValue}
+              onValueChange={setUrlInputValue}
+              disabled={isReady}
+              autoComplete="off"
+              color={isReady ? "success" : "default"}
+            />
+            <Button
+              isIconOnly
+              aria-label="go"
+              color={isReady ? "danger" : "secondary"}
+              className="text-white h-14 w-14 rounded-full"
+              onClick={handleUrlChange}
+            >
+              <div className="text-4xl">🐼</div>
+            </Button>
+          </motion.div>
+          {isReady && (
+            <motion.div
+              key="chat-message-table"
+              layout
+              initial={{ opacity: 0, y: 50 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5 }}
+              className="h-[65vh]"
+            >
+              <div className="flex gap-2 pl-[4.5rem] items-center bg-[#18181b] rounded-tl-lg rounded-tr-lg py-2 relative">
+                <div className="text-center w-[200px]">NAME</div>
+                <div className="text-center w-full">MESSAGE</div>
+              </div>
+              <Table
+                aria-label="yt-chat-message-table"
+                selectedKeys={readByeBye}
+                onSelectionChange={handleReadByeBye}
+                selectionMode="multiple"
+                color="success"
+                classNames={{
+                  base: "max-h-[100%] overflow-hidden",
+                  table: "min-h-[25rem] -mt-12",
+                  thead: "invisible",
+                  wrapper: "rounded-br-none rounded-tr-none rounded-tl-none",
+                }}
+                isHeaderSticky
+              >
+                <TableHeader aria-label="header" columns={columns}>
+                  {(column) => (
+                    <TableColumn aria-label="column" key={column.key} width={column.width}>
+                      {column.label}
+                    </TableColumn>
+                  )}
+                </TableHeader>
+                <TableBody items={rows}>
+                  {(item) => (
+                    <TableRow aria-label="row" key={item.key}>
+                      {(columnKey) => (
+                        <TableCell aria-label="cell">{getKeyValue(item, columnKey)}</TableCell>
+                      )}
+                    </TableRow>
+                  )}
+                </TableBody>
+              </Table>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </motion.div>
     </main>
-  )
+  );
 }
