296d35415fc02769cf7f3e42c2bd31f906b37c8d
Sawa <34698182+sawaYch@users.noreply.github.com>
2023-09-19 15:04:33 +0800

    fix: apple pwa startup image

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Image from "next/image";
+import Head from "next/head";
 import { Providers } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -235,6 +236,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="dark">
+      <Head>
+        <link
+          rel="icon"
+          href="/icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="/apple-icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+      </Head>
       <body className={inter.className}>
         <div className="fixed top-0 left-0 w-screen h-screen overflow-hidden">
           <Image
